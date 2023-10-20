39d1699dff9dba9e51553abdf994091fb40e42e8
yeferson <yefersonapk@gmail.com>
2023-10-20 11:07:06 -0500

    four

--- src/app.js
+++ src/app.js
@@ -4,12 +4,18 @@ import employeesRoutes from "./routes/employees.routes.js";
 
 import indexRoutes from "./routes/index.router.js";
 
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 
+app.use(cors());
+
 app.use("/api/employees", employeesRoutes);
+
+
+
 app.use("/", indexRoutes);
 
 app.use((req, res, next) => {
